b518f347afcbe4f4aab8a4bd2431841659f52260
AghedoEmmanuel <emmaodianose@gmail.com>
2025-06-08 11:31:28 +0100

    navigation bar added

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,12 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const MovieDetails = () => {
+const Saved = () => {
   return (
     <View>
-      <Text>MovieDetails</Text>
+      <Text>Saved</Text>
     </View>
   )
 }
 
-export default MovieDetails
+export default Saved
--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,12 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const onBoarding = () => {
+const Search = () => {
   return (
     <View>
-      <Text>onBoarding</Text>
+      <Text>S</Text>
     </View>
   )
 }
 
-export default onBoarding
+export default Search
