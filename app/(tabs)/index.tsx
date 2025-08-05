08bbea1a9ae32143e8ce0a5a909b4bb88f64b951
AghedoEmmanuel <emmaodianose@gmail.com>
2025-08-05 23:16:38 +0100

    working on showing trending movies

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,80 +1,109 @@
+import MovieCard from "@/components/MovieCard";
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
-import { ActivityIndicator, FlatList, Image, ScrollView, Text, View } from "react-native";
-import { useRouter } from "expo-router";
-import useFetch from "@/services/useFetch";
 import { fetchMovies } from "@/services/api";
-import MovieCard from "@/components/MovieCard";
+import { getTrendingMovies } from "@/services/appwrite";
+import useFetch from "@/services/useFetch";
+import { useRouter } from "expo-router";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  ScrollView,
+  Text,
+  View,
+} from "react-native";
 
 export default function Index() {
-
   const router = useRouter();
 
   const {
-    data:movies, 
-    loading:moviesLoading, 
-    error:moviesError} = 
-    useFetch(() => fetchMovies({
-    query:'',
-  }))
+    data: trendingMovies,
+    loading: trendingMoviesLoading,
+    error: trendingMoviesError,
+  } = useFetch(getTrendingMovies);
 
-  const renderHeader = ()=>{
-    return(
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() =>
+    fetchMovies({
+      query: "",
+    })
+  );
+
+  const renderHeader = () => {
+    return (
       <View>
-         <Image
-        source={icons.logo}
-        className="w-12 h-10 mt-20 mb-5 mx-auto"
-        />
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
         <SearchBar
-          onPress={() => router.push('/search')}
+          onPress={() => router.push("/search")}
           placeholder="Search for a movie"
-          />
-          <Text className="text-lg text-white font-bold mt-5 mb-3">Latest Movies</Text>
+        />
+        <Text className="text-lg text-white font-bold mt-5 mb-3">
+          Latest Movies
+        </Text>
       </View>
-    )
-  }
+    );
+  };
 
   return (
     <View className="flex-1 bg-primary">
-      <Image
-      source={images.bg}
-      className="absolute w-full z-0"
+      <Image source={images.bg} className="absolute w-full z-0" />
+       {trendingMovies && (
+        <View className="mt-10">
+          <Text className="text-lg text-white font-bold">Trending Movies</Text>
+        </View>
+      )}
+      <FlatList
+        className="mb-4 mt-3"
+        data={trendingMovies}
+        renderItem={({ item}) => (
+          <Text className="text-white text-sm">{item.title}</Text>
+        )}
       />
       <FlatList
-      ListHeaderComponent={renderHeader}
-      data={movies}
-      renderItem={({item})=>(
-        <MovieCard 
-        {...item}
-        />
-      )}
-      keyExtractor={(item)=>item.id}
-      numColumns={3}
-      showsVerticalScrollIndicator={false}
+        ListHeaderComponent={renderHeader}
+        data={movies}
+        renderItem={({ item }) => <MovieCard {...item} />}
+        keyExtractor={(item) => item.id}
+        numColumns={3}
+        showsVerticalScrollIndicator={false}
         columnWrapperStyle={{
-          justifyContent:'flex-start',
-          gap:20,
+          justifyContent: "flex-start",
+          gap: 20,
           paddingRight: 5,
           marginBottom: 10,
         }}
-        contentContainerStyle={{paddingLeft:5, paddingRight:5}}
+        contentContainerStyle={{ paddingLeft: 5, paddingRight: 5 }}
         className="mt-2 pb-32"
         scrollEnabled={true}
         ListEmptyComponent={() => (
-          <ScrollView className="flex-1 mt-5 "
-          showsVerticalScrollIndicator={false}
-          contentContainerStyle={{alignItems:'center', justifyContent:'center'}}>
-            {moviesLoading ? (
-              <ActivityIndicator size={'large'} color={'#0000ff'} />
-            ) : moviesError ? (
-              <Text>Error: {moviesError?.message}</Text>
+          <ScrollView
+            className="flex-1 mt-5 "
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            {moviesLoading || trendingMoviesLoading ? (
+              <ActivityIndicator size={"large"} color={"#0000ff"} />
+            ) : moviesError || trendingMoviesError ? (
+              <Text>
+                Error: {moviesError?.message || trendingMoviesError?.message}
+              </Text>
             ) : (
               <Text>No movies found.</Text>
             )}
           </ScrollView>
         )}
       />
+
+     
+
       {/* <ScrollView className="flex-1 px-5"
       showsVerticalScrollIndicator={false}
       contentContainerStyle={{minHeight: '100%', paddingBottom: 10}}
